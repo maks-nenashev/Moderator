4c179feeab8f8ae8e30e44a67826a2ba66f2bde4
Maksym <57881489+maks-nenashev@users.noreply.github.com>
2026-01-07 02:44:16 +0100

    v2 stab

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.routes import router as api_router
 from app.ml.state import model_loader
 from app.ml.state_v2 import model_loader_v2 # load second model state
+from app.ml.state_v3 import model_loader_v3     # load third model state
 
 app = FastAPI(
     title="Moderator",
@@ -16,6 +17,7 @@ app.include_router(api_router)
 def startup_event():
     model_loader.load()
     model_loader_v2.load() # load second model on startup
+    #model_loader_v3.load()  # load third model on startup
 
 @app.get("/")
 def root():
